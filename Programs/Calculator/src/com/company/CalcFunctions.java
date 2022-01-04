749e8d3c6280201b0b32f307c58707a6ad8c600e
AHealy16 <AH211841@reigate.ac.uk>
2022-01-04 16:10:38 +0000

    Objects

---
+++ Programs/Calculator/src/com/company/CalcFunctions.java
@@ -0,0 +1,35 @@
+package com.company;
+import java.util.Scanner;
+public class CalcFunctions {
+
+    public static int addNums(int a, int b){
+
+        int total = a + b;
+        return total;
+
+    }
+
+    public static int subtractNums(int a, int b){
+
+        int total = a - b;
+        return total;
+    }
+
+    public static int multiplyNums(int a, int b){
+
+        int total = a * b;
+        return total;
+    }
+
+    public static int divideNums(int a, int b) {
+
+        int total = a / b;
+        return total;
+    }
+
+    public static int exponentiationNums(int a, int b){
+
+
+    }
+
+}
---
+++ Programs/Calculator/src/com/company/CalcInput.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+public class CalcInput {
+
+    public static
+
+
+
+
+    }
+
+
+
+}
